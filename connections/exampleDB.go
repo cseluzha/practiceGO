0f4336cb8495d544da12741bf26c8a9b6dfb4127
Chalchiuhcuauhtli <ricardo6582@gmail.com>
2022-01-17 14:56:53 -0600

    feat: test connect with mysql in local data base

---
+++ connections/exampleDB.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"database/sql"
+	"time"
+	_ "github.com/go-sql-driver/mysql"
+	"fmt"
+)
+
+func main() {
+
+	db, err := sql.Open("mysql", "root:chalchi@123@/asainexus")	
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	// Execute the query
+	rows, err := db.Query("SELECT * FROM User WHERE IdUser in (3, 49,80)")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	// Get column names
+	columns, err := rows.Columns()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	// Make a slice for the values
+	values := make([]sql.RawBytes, len(columns))
+
+	// rows.Scan wants '[]interface{}' as an argument, so we must copy the
+	// references into such a slice
+	// See http://code.google.com/p/go-wiki/wiki/InterfaceSlice for details
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	// Fetch rows
+	for rows.Next() {
+		// get RawBytes from data
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+
+		// Now do something with the data.
+		// Here we just print each column as a string.
+		var value string
+		for i, col := range values {
+			// Here we can check if the value is nil (NULL value)
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			fmt.Println(columns[i], ": ", value)
+		}
+		fmt.Println("-----------------------------------")
+	}
+	if err = rows.Err(); err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+}
