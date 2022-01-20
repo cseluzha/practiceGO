70c112f64f23edf9fe37213a10ba89839a140115
Chalchiuhcuauhtli <ricardo6582@gmail.com>
2022-01-20 16:27:05 -0600

    feat: add the validate user when create a reminder

--- reminders/cmd/api/handlers/schedulehandler.go
+++ reminders/cmd/api/handlers/schedulehandler.go
@@ -2,12 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
+	uuid "github.com/google/uuid"
+	"github.com/labstack/echo/v4"
 	"log"
 	"net/http"
 	"reminders/cmd/api/vo"
 	"reminders/internal/repository"
-
-	"github.com/labstack/echo/v4"
 )
 
 func NewSchedule(c echo.Context) error {
@@ -18,15 +18,22 @@ func NewSchedule(c echo.Context) error {
 		log.Fatalf("Failed reading the request body %s", err)
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error)
 	}
-	log.Printf("this is your schedule %#v", schedule)
-	s := repository.NewScheduleRepository()
-	scheduleId := s.NewSchedule(repository.Schedule{
-		Id:          repository.GenerateUUID(),
-		Description: schedule.Description,
-		Users:       schedule.Users,
-	})
-	log.Printf("The new schedule id is %v", scheduleId)
-	return c.String(http.StatusOK, "schedule created successfully!")
+	res := checkUsers(schedule.Users)
+	if res {
+		s := repository.NewScheduleRepository()
+		scheduleId := s.NewSchedule(repository.Schedule{
+			Id:          repository.GenerateUUID(),
+			Description: schedule.Description,
+			Users:       schedule.Users,
+		})
+		log.Printf("The new schedule id is %v", scheduleId)
+		//TODO: validate if it was created correctly
+		return c.String(http.StatusOK, "Schedule created successfully!")
+	} else {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "One or more users do not exist",
+		})
+	}
 }
 
 func UpdateSchedule(c echo.Context) error {
@@ -80,4 +87,29 @@ func GetSchedules(c echo.Context) error {
 			"error": "Any Error",
 		})
 	}
+}
+
+func checkUsers(users []uuid.UUID) bool {
+	ur := repository.NewUserRepository()
+	usersDb, _ := ur.ListUsers()
+	var result bool = false
+	for _, u := range users {
+		result = contains(usersDb, u)
+		if !result {
+			break
+		}
+	}
+	return result
+}
+
+//TODO: Investigate if exit other form better 
+func contains(s []repository.User, str uuid.UUID) bool {
+	var r bool = false
+	for _, v := range s {
+		if v.IdUser == str {
+			r = true
+			break
+		}
+	}
+	return r
 }
